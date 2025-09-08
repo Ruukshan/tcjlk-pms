4ba8f56f191824b6bdb78f7b7660e4fef61c11fc
sahansasindu <sahan_2020742@fot.ruh.ac.lk>
2025-09-08 22:25:22 +0530

    add

---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="navbar">
+    <button class="back-btn" @click="goBack">
+      ←
+    </button>
+    <h2 class="title">{{ title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  props: {
+    title: {
+      type: String,
+      default: "Material Receipt"
+    }
+  },
+  methods: {
+    goBack() {
+      // Go back to previous page
+      window.history.back();
+    }
+  }
+};
+</script>
