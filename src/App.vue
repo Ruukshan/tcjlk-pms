cea2bcd3f58b4542ae02a1614b99e189670d2589
osandaDev <osanda1208@gmail.com>
2025-09-09 21:57:07 +0530

    update

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <v-app>
-    <TjcPackingPage />
-     <TjcGrnPage /> 
+   <!--<TjcPackingPage />-->
+     <!--<TjcGrnPage /> -->
+    <TjcProcessingPage />
   </v-app>
 </template>
 
 <script setup>
 import TjcGrnPage from "./views/TjcGrnPage.vue";
 import TjcPackingPage from "./views/TjcPackingPage.vue";
+import TjcProcessingPage from "./views/TjcProcessingPage.vue";
 </script>
 
 <style scoped>
