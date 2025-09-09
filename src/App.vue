3561a8f26d2f5b6b0bcb7fd39ef2d04783f8b2aa
osandaDev <osanda1208@gmail.com>
2025-09-09 22:43:44 +0530

    refactor: remove unused components and delete vuetify-custom.css

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <template>
   <v-app>
-   <!--<TjcPackingPage />-->
-     <!--<TjcGrnPage /> -->
+   
     <TjcProcessingPage />
   </v-app>
 </template>
 
 <script setup>
-import TjcGrnPage from "./views/TjcGrnPage.vue";
-import TjcPackingPage from "./views/TjcPackingPage.vue";
 import TjcProcessingPage from "./views/TjcProcessingPage.vue";
 </script>
 
